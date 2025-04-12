177f43e4db67f2a970f92eb7a74376382797d0dd
Orestis Tsatsos <orestis@Orestiss-Air.home>
2025-04-12 20:15:37 +0300

    complete game functionalities

--- dummy-rock-paper-scissors/script.js
+++ dummy-rock-paper-scissors/script.js
@@ -1,12 +1,15 @@
 const options = ["Rock", "Paper","Scissors"];
+const playerScoreSpan = document.getElementById("player-score-number");
+const computerScoreSpan = document.getElementById("computer-score-number");
 const btnRock = document.getElementById("option1-btn");
 const btnPaper = document.getElementById("option2-btn");
 const btnScissors = document.getElementById("option3-btn");
 const roundWinnerMsg = document.getElementById("round-results-msg");
 const roundResultsDiv = document.getElementById("round-results");
-const playerScoreSpan = document.getElementById("player-score-number");
-const computerScoreSpan = document.getElementById("computer-score-number");
-let playerOption;
+const gameWinnerDiv = document.getElementById("game-results")
+const gameWinnerMsg = document.getElementById("game-results-msg");
+const optionsDiv = document.getElementById("options");
+const btnReset = document.getElementById("reset-button");
 let playerScore=0;
 let computerScore=0;
 
@@ -24,6 +27,7 @@ btnScissors.addEventListener("click", function() {
     roundResults("Scissors");
 });
 
+btnReset.addEventListener("click",resetGame);
 
 function computerOption(){
     return options[Math.floor(Math.random()*options.length)];
@@ -42,4 +46,32 @@ function roundResults(player){
     } 
     playerScoreSpan.innerText=playerScore;
     computerScoreSpan.innerText=computerScore;
+    gameWin(playerScore,computerScore);
+}
+
+function gameWin(player,computer){
+    let win=false;
+    if(player>2){
+        gameWinnerMsg.innerText="Player wins the game!";
+        win=true;
+    } else if (computer>2){
+        gameWinnerMsg.innerText="Computer wins the game!";
+        win=true;
+    }
+    if(win){
+        gameWinnerDiv.style.display="block";
+        optionsDiv.style.display="none";
+        btnReset.style.display="block";
+    }
+}
+
+function resetGame(){
+    gameWinnerDiv.style.display="none";
+    optionsDiv.style.display="block";
+    btnReset.style.display="none";
+    playerScore=0;
+    computerScore=0;
+    playerScoreSpan.innerText=playerScore;
+    computerScoreSpan.innerText=computerScore;
+    roundWinnerMsg.innerText="";
 }
